b8291e89b2cdd3af8abf7cf47f247dd499a345b8
Fahim sahriar <fahimsahriar2002@gmail.com>
2026-08-13 08:11:37 +0600

    add user authentication with OAuth2 password form

--- router/auth.py
+++ router/auth.py
@@ -1,12 +1,15 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends, HTTPException, status
 from pydantic import BaseModel
 from models import Users
 from passlib.context import CryptContext
 from sqlalchemy.orm import Session
 from typing import Annotated 
 from database import SessionLocal
-from fastapi import Depends
 from fastapi.responses import JSONResponse
+from fastapi.security import OAuth2PasswordRequestForm 
+
+
+
 
 router = APIRouter()
 
@@ -31,6 +34,15 @@ class CreateUsers(BaseModel):
     password: str
     role: str
 
+def authenticate_user(username: str, password: str, db: Session):
+    user = db.query(Users).filter(Users.username == username).first()
+
+    if not user:
+        return False
+    if not bcrypt_context.verify(password, user.hash_password):
+        return False
+
+    return user
 
 @router.post("/createuser")
 def create_users(db:db_dependency,new_user: CreateUsers):
@@ -51,4 +63,18 @@ def create_users(db:db_dependency,new_user: CreateUsers):
     return JSONResponse(
     status_code=201,
     content={"message": "User created successfully"}
-)
+) 
+    
+
+@router.post("/login")
+def login_user(db: db_dependency, form_data: Annotated[OAuth2PasswordRequestForm, Depends()]):
+    user = authenticate_user(form_data.username, form_data.password, db)
+
+    if not user:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Username or password is incorrect"
+        )
+
+    return {"message": "Authenticated User", "username": user.username}
+    
