4b481301ff4bb249eab9f9298e07026cf86f19a0
Fahim sahriar <fahimsahriar2002@gmail.com>
2026-08-16 12:05:22 +0600

    configure PostgreSQL database connection

--- database.py
+++ database.py
@@ -1,26 +1,22 @@
-# SQLAlchemy দিয়ে Database connection ও configuration setup
-
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
+from dotenv import load_dotenv
 
+load_dotenv()
 
-# SQLite database URL
-SQLALCHEMY_DATABASE_URL = "sqlite:///./todoapp.db"
-
+# PostgreSQL database URL (loaded from .env)
+SQLALCHEMY_DATABASE_URL = os.getenv("DATABASE_URL")
 
 # Create database engine
-engine = create_engine(
-    SQLALCHEMY_DATABASE_URL,
-    connect_args={"check_same_thread": False}
-)
- 
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
+
 # Create a local database session
 SessionLocal = sessionmaker(
     autocommit=False,
     autoflush=False,
     bind=engine
 )
-
 
 # Base class for SQLAlchemy models
 Base = declarative_base()
